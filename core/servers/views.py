f9b4f4c53d62e262f217fb1e4299c6c1fc33a132
Andrey Velikanov <andreyvelikanovv@gmail.com>
2024-10-15 11:13:12 +0400

    fr Commit

--- core/servers/views.py
+++ core/servers/views.py
@@ -3,12 +3,12 @@ from rest_framework.response import Response
 from core.servers.models import OutlineServerKey
 from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView
+from django.http import JsonResponse
 import logging
 
 logger = logging.getLogger(__name__)
 
 class GetConfigByKeyViewSet(APIView):
-    renderer_classes = [JSONRenderer]
 
     def get(self, request):
         try:
@@ -20,6 +20,6 @@ class GetConfigByKeyViewSet(APIView):
                 "password": server_key.password,
                 "method": server_key.method
             }
-            return Response(data)
+            return JsonResponse(data)
         except:
             return Response({"status": False})
