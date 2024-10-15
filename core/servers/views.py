7aa896de449e7fe8b76049cd34ed26337143dd2f
Andrey Velikanov <andreyvelikanovv@gmail.com>
2024-10-15 10:59:11 +0400

    fr Commit

--- core/servers/views.py
+++ core/servers/views.py
@@ -1,8 +1,11 @@
+from django.http import JsonResponse
 from rest_framework.response import Response
 from core.servers.models import OutlineServerKey
 from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView
+from django.http import HttpResponse
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -19,8 +22,14 @@ class GetConfigByKeyViewSet(APIView):
                 "password": server_key.password,
                 "method": server_key.method
             }
-            logger.info(str(data))
-            return Response(data)
+            # Преобразуем данные в JSON-строку с отступами
+            json_data = json.dumps(data, indent=4)
+
+            # Создаем HTTP-ответ с типом содержимого 'application/json'
+            response = HttpResponse(json_data, content_type='application/json')
+
+            # Добавляем заголовок для предложения файла к скачиванию
+            response['Content-Disposition'] = 'attachment; filename="data.json"'
+            return response
         except:
-            logger.info(str({"status": False}))
             return Response({"status": False})
