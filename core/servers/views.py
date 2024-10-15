7c9e35535a3316fe988147be6ac6bf207b1ebe4d
Andrey Velikanov <andreyvelikanovv@gmail.com>
2024-10-15 11:25:42 +0400

    fr Commit

--- core/servers/views.py
+++ core/servers/views.py
@@ -4,6 +4,7 @@ from core.servers.models import OutlineServerKey
 from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView
 from django.http import JsonResponse
+import hashlib
 import logging
 
 logger = logging.getLogger(__name__)
@@ -20,6 +21,22 @@ class GetConfigByKeyViewSet(APIView):
                 "password": server_key.password,
                 "method": server_key.method
             }
-            return JsonResponse(data)
+            # Преобразуем данные ответа в строку для хеширования
+            content_str = str(data).encode('utf-8')
+
+            # Создаем хеш для ETag
+            etag = hashlib.md5(content_str).hexdigest()
+
+            # Проверяем, отправлял ли клиент заголовок If-None-Match
+            if_none_match = request.headers.get('If-None-Match')
+
+            if if_none_match == etag:
+                # Возвращаем 304 Not Modified, если ETag совпадает
+                return Response(status=304)
+
+            # Возвращаем ответ с ETag
+            response = Response(data)
+            response['ETag'] = etag
+            return Response(data)
         except:
             return Response({"status": False})
