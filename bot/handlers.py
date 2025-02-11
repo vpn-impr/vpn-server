5c229c8a882261c1bfdb3b8dad68a63ca9f02f1f
Andrey Velikanov <andreyvelikanovv@gmail.com>
2025-02-11 11:32:44 +0400

    add myhome1-6

--- bot/handlers.py
+++ bot/handlers.py
@@ -386,7 +386,6 @@ async def handle_myhome_get_image_command(message: Message) -> None:
             output.close()
         if need_add_to_db:
             async with aiohttp.ClientSession() as session:
-                await message.answer("Добавляем в систему")
                 url_aur = 'https://api.rem.auora-estate.ge/v1/pre_approve_by_myhome/approve/'
                 headers = {'Authorization': 'Token f4254f25dde331cac97959872d614eaaef7ca2a2'}
                 try:
